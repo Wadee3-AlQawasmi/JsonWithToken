9e06a356089b6f6b2f28a274bd5bdcd9be244520
wadee3_AlQawasmi <wadee_yousef@yahoo.com>
2022-01-15 23:04:54 +0200

    {JWT}

---
+++ JWTAUTH/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAUTH.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username Is Required")]
+        public string Username { set; get; }
+
+        [Required(ErrorMessage = "Password Is Required")]
+        public string Password { set; get; }
+    }
+}
---
+++ JWTAUTH/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAUTH.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage ="Username Is Required")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email Is Required")]
+        public string Email { set; get; }
+
+        [Required(ErrorMessage = "Password Is Required")]
+        public string Password { set; get; }
+    }
+}
